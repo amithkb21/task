b4f77b1a69e66c86d5d207aa3a2d6bb6da9257ac
amit <amit123@gmail.com>
2022-12-27 16:27:04 +0000

    file edited

--- replace.sh
+++ replace.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-git pull origin master
+#!/bin/bash
 
-find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \;
+origin="origin"
+branch="deploy"
+new_commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
+echo $commit_id_latest
 
-find /var/www/html/wl2/testing/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=TEST/g' {} \;
+old_commit_id=$(</home/amitayare/file1.txt) 
+
+
+if [ $new_commit_id == $old_commit_id ];
+
+then 
+	echo " NO new commits "
+else
+	echo " new commit id is FOUND "
+	
+	find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
+	find /var/www/html/wl2/testing/ -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
+	
+	git add --all
+	git commit -m " COMMIT CODE "
+	git push origin test
+	
+	git checkout test
+	git merge deploy
+
+#testing
+
+commit_save=$new_commit_id
+echo $new_commit_id > /home/amitayare/file1.txt 
+echo $commit_save
+
+fi
+
+
 
 
 
