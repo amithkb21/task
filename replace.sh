a31f7998f3286d2d03db237187020d6735a42fdc
amit <amit123@gmail.com>
2022-12-27 17:45:39 +0000

    COMMIT CODE

--- replace.sh
+++ replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 origin="origin"
-branch="deploy"
+branch="dev"
 new_commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
 echo $commit_id_latest
 
@@ -15,15 +15,15 @@ then
 else
 	echo " FOUND : NEW COMMIT  "
 	
-	find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
-	find /var/www/html/wl2/testing/ -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
+#	find /var/www/html/wl2/dev/ -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
+#	find /var/www/html/wl2/testing/ -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
 	
 	git add --all
 	git commit -m " COMMIT CODE "
-	git push origin test
+#	git push origin test
 	
-	git checkout test
-	git merge deploy
+#	git checkout test
+	git merge dev
 
 #testing
 
@@ -32,10 +32,4 @@ echo $new_commit_id > /home/amitayare/file1.txt
 echo $commit_save
 
 fi
-
-
-
-
-
-
 
