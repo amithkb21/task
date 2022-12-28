88ea36cc6275ff9999066ecaf2aa0d0d6f3fabef
amithkb21 <121281179+amithkb21@users.noreply.github.com>
2022-12-28 10:05:18 +0530

    replace file changes

--- replace.sh
+++ replace.sh
@@ -3,37 +3,38 @@
 origin="origin"
 branch="dev"
 
+#new_commit_id
 new_commit_id=$(git log -n 1 --pretty=format:%H "$origin/$branch")
 echo " new_commit_id: $new_commit_id"
 
 old_commit_id=$(</var/www/html/task/file1.txt) 
-git branch
+
 echo " commit old: $old_commit_id "
+
+#compare old and new commit id 
 
 if [ $new_commit_id == $old_commit_id ];
 
-then 
-	echo " NO NEW COMMIT  "
-else
-	echo " FOUND  NEW COMMIT  "
-	
-	
-	
-	find /var/www/html/task/dev/ -name "*.env" -exec sed -i 's/LOCAL/DEV/g' {} \;
-	find /var/www/html/task/testing/ -name "*.env" -exec sed -i 's/LOCAL/TEST/g' {} \;
-	
-	git add --all
-	git commit -m " COMMIT CODE "
-#	git push origin test
-	
-#	git checkout test
-	git branch
-	git merge dev
+	then 
+		echo " NO NEW COMMIT  "
+	else
+		echo " FOUND  NEW COMMIT  "
 
-#testing
+		find /var/www/html/task/dev/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=DEV/g' {} \;
+		find /var/www/html/task/testing/ -name "*.env" -exec sed -i 's/APP_ENV=LOCAL/APP_ENV=LOCAL/g' {} \;
 
-commit_save=$new_commit_id
-echo $new_commit_id > file1.txt 
-echo $commit_save
+		git add --all
+		git commit -m " COMMIT CODE "
+		
+#git push origin test
+#git checkout test
+
+		git merge dev
+
+	#backup_old_id 
+
+	commit_save=$new_commit_id
+	echo $new_commit_id > file1.txt 
+	echo $commit_save
 
 fi
